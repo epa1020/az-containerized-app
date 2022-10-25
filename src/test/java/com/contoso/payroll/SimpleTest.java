ecee2c643128fbaa6fbf3c2ca81b089b26bdb52d
epa1020 <edward@example.es>
2022-10-25 01:20:42 -0600

    init project

---
+++ src/test/java/com/contoso/payroll/SimpleTest.java
@@ -0,0 +1,11 @@
+package com.contoso.payroll;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+    @Test
+    void verifyTest(){
+        assertEquals(7,7);
+    }
+}
